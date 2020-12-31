b88723ae01905a13e83b32408a9341d3e60a1522
IOE <fivetwothreesix@gmail.com>
2021-01-01 01:28:57 +0900

    update

--- nodec_extentions/nodec_serialization/unit_tests/test_basic/test_basic.cpp
+++ nodec_extentions/nodec_serialization/unit_tests/test_basic/test_basic.cpp
@@ -1,7 +1,8 @@
 #include <nodec_extentions/nodec_serialization/vector2.hpp>
 #include <nodec_extentions/nodec_serialization/quaternion.hpp>
+#include <nodec_extentions/nodec_serialization/nodec_object.hpp>
 
-#include <nodec/quaternion.hpp>
+#include <nodec/logging.hpp>
 
 #include <cereal/types/unordered_map.hpp>
 #include <cereal/types/memory.hpp>
@@ -47,18 +48,37 @@ struct SomeData
 
 int main()
 {
-    std::ofstream os("out.cereal", std::ios::binary);
-    cereal::JSONOutputArchive archive(os);
+    logging::record_handlers += logging::StaticRecordHandler::make_shared(&logging::record_to_stdout_handler);
 
-    SomeData myData;
-    myData.data = std::make_shared<std::unordered_map<uint32_t, MyRecord>>();
-    myData.data->emplace(0, MyRecord());
+    {
+        std::ofstream os("out.cereal", std::ios::binary);
+        cereal::JSONOutputArchive archive(os);
 
-    auto vec = std::make_shared<Vector2f>(0, 1);
-    Quaternionf quaternion;
-    
-    archive(myData, vec, quaternion);
+        SomeData myData;
+        myData.data = std::make_shared<std::unordered_map<uint32_t, MyRecord>>();
+        myData.data->emplace(0, MyRecord());
 
+        auto vec = std::make_shared<Vector2f>(0, 1);
+        Quaternionf quaternion;
+
+        auto nodec_object = NodecObject::instanciate<NodecObject>("test");
+
+        //archive(myData, vec, quaternion, nodec_object);
+        archive(vec, quaternion, nodec_object);
+    }
+    {
+        std::ifstream is("out.cereal", std::ios::binary);
+        cereal::JSONInputArchive archive(is);
+
+        NodecObject::Holder<NodecObject> nodec_object;
+        std::shared_ptr<Vector2f> vec;
+        Quaternionf quaternion;
+
+        archive(vec, quaternion, nodec_object);
+
+        logging::DebugStream(__FILE__, __LINE__) << *vec << quaternion << nodec_object->name << nodec_object->id();
+
+    }
 
     return 0;
 }
