fd12f3c97ca6f38cd06aec94e90d0435491380bf
IOE <fivetwothreesix@gmail.com>
2022-01-16 19:29:39 +0900

    update test

--- nodec/unit_tests/thread_pool_executor/test_basic/main.cpp
+++ nodec/unit_tests/thread_pool_executor/test_basic/main.cpp
@@ -18,8 +18,45 @@ std::string counter(int timeout) {
     return { "OK!!" };
 }
 
+class HelloWorld {
+public:
+    std::string say_hello(int number) {
+        std::cout << "[say_hello (" << std::this_thread::get_id() << ")] >>> Start" << std::endl;
+        std::this_thread::sleep_for(std::chrono::milliseconds(2000));
+        std::ostringstream oss;
+        oss << "[say_hello (" << std::this_thread::get_id() << ")] >>> Hello! number: " << number;
+        std::cout << "[say_hello (" << std::this_thread::get_id() << ")] >>> End" << std::endl;
+        return oss.str();
+    }
+};
+
+std::string say_ok(int number) {
+    std::cout << "[say_ok (" << std::this_thread::get_id() << ")] >>> Start" << std::endl;
+    std::this_thread::sleep_for(std::chrono::milliseconds(3000));
+    std::ostringstream oss;
+    oss << "[say_ok (" << std::this_thread::get_id() << ")] >>> OK! number: " << number;
+    std::cout << "[say_ok (" << std::this_thread::get_id() << ")] >>> End" << std::endl;
+    return oss.str();
+}
+
 int main() {
     using namespace nodec;
+
+    {
+        concurrent::ThreadPoolExecutor executor;
+
+        std::cout << "thread_count: " << executor.thread_count() << std::endl;
+
+        auto ok_future = executor.submit(say_ok, 100);
+
+        HelloWorld hello;
+        auto hello_future = executor.submit([&](auto number){ return hello.say_hello(number); }, 999);
+
+        std::cout << ok_future.get() << std::endl;
+        std::cout << hello_future.get() << std::endl;
+
+        return 0;
+    }
 
     logging::record_handlers().connect(logging::record_to_stdout_handler);
 
@@ -50,7 +87,7 @@ int main() {
                 //executor.submit(counter, 12);
                 counter(3);
                 return std::this_thread::get_id();
-                                          });
+                });
 
             //logging::InfoStream(__FILE__, __LINE__) << future.get();
         }
