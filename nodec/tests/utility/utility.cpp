e5dd82e064ae78f26cc16140b4dc882f98d7403d
IOE <fivetwothreesix@gmail.com>
2022-12-29 19:33:03 +0900

    fix tests.

--- nodec/tests/utility/utility.cpp
+++ nodec/tests/utility/utility.cpp
@@ -9,11 +9,11 @@ TEST_CASE("Testing to_underlying.") {
     // https://en.cppreference.com/w/cpp/utility/to_underlying
 
     enum class E1 : char { e };
-    static_assert(std::is_same_v<char, decltype(nodec::to_underlying(E1::e))>, "failed.");
+    static_assert(std::is_same<char, decltype(nodec::to_underlying(E1::e))>::value, "failed.");
 
     enum struct E2 : long { e };
-    static_assert(std::is_same_v<long, decltype(nodec::to_underlying(E2::e))>, "failed.");
+    static_assert(std::is_same<long, decltype(nodec::to_underlying(E2::e))>::value, "failed.");
 
     enum E3 : unsigned { e };
-    static_assert(std::is_same_v<unsigned, decltype(nodec::to_underlying(e))>, "failed.");
+    static_assert(std::is_same<unsigned, decltype(nodec::to_underlying(e))>::value, "failed.");
 }
