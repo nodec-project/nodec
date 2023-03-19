6f2b7844f0a8782705c2f2a0fb6c3582a33578d1
IOE <fivetwothreesix@gmail.com>
2023-03-19 21:11:21 +0900

    impl EntitySerializer.

--- modules/nodec_scene_serialization/include/nodec_scene_serialization/entity_serializer.hpp
+++ modules/nodec_scene_serialization/include/nodec_scene_serialization/entity_serializer.hpp
@@ -2,27 +2,71 @@
 #define NODEC_SCENE_SERIALIZATION__ENTITY_SERIALIZER_HPP_
 
 #include "scene_serialization.hpp"
+#include "serializable_entity.hpp"
+
+#include <nodec_scene/components/hierarchy.hpp>
+#include <nodec_scene/scene.hpp>
+
+#include <stack>
 
 namespace nodec_scene_serialization {
 
 class EntitySerializer final {
 public:
     EntitySerializer(const nodec_scene::SceneEntity &entity, const nodec_scene::Scene &scene, const SceneSerialization &serialization)
-        : serialization_(serialization) {}
+        : serialization_(serialization), scene_(scene), target_entity_(entity) {}
 
-    std::shared_ptr<SerializableEntity> serialize(const nodec_scene::SceneEntity &entity, const nodec_scene::Scene &scene) const {
-        auto root = serialization_.make_serializable_entity(entity, scene.registry());
+    std::unique_ptr<SerializableEntity> serialize() const {
+        using namespace nodec::entities;
+        using namespace nodec_scene;
+        using namespace nodec_scene::components;
 
-        // TODO: Push the root entity.
-        // TODO: while the stack is not empty.
-        // TODO:    Pop the entity from stack.
-        // TODO:    Get hierarchy component.
-        // TODO:    Get child entities.
-        // TODO:    Foreach all children.
-        // TODO:        Serialize child.
-        // TODO:        Append child to current entity.
-        // TODO:        Push child to the stack.
-        // TODO: Return root entity.
+        // If the entity is null, the process does not continue.
+        // This is to guarantee that the null entity will not be included in the context that follows.
+        if (target_entity_ == null_entity) return {};
+
+        struct Context {
+            SceneEntity entity;
+            SerializableEntity *parent;
+        };
+
+        const auto &scene_registry = scene_.registry();
+        std::unique_ptr<SerializableEntity> root;
+
+        std::stack<Context> stack;
+        stack.push({target_entity_, nullptr});
+
+        while (!stack.empty()) {
+            auto context = stack.top();
+            stack.pop();
+
+            SerializableEntity *ser_entity_ptr;
+            {
+                auto ser_entity = serialization_.make_serializable_entity(context.entity, scene_registry);
+                ser_entity_ptr = ser_entity.get();
+                if (!root) {
+                    // since this entity is root, it should have no parent.
+                    assert(context.parent == nullptr);
+                    root = std::move(ser_entity);
+                } else {
+                    assert(context.parent != nullptr);
+                    context.parent->children.emplace_back(std::move(ser_entity));
+                }
+            }
+
+            auto *hierarchy = scene_registry.try_get_component<Hierarchy>(context.entity);
+            if (hierarchy == nullptr) continue;
+
+            auto child = hierarchy->first;
+            ser_entity_ptr->children.reserve(hierarchy->child_count);
+            while (child != null_entity) {
+                stack.push({child, ser_entity_ptr});
+                // The hierarchy should be contained since the parent has the hierarchy.
+                child = scene_registry.get_component<Hierarchy>(child).next;
+            }
+        }
+
+        return root;
     }
 
 private:
--- modules/nodec_scene_serialization/tests/entity_serializer.cpp
+++ modules/nodec_scene_serialization/tests/entity_serializer.cpp
@@ -1,3 +1,30 @@
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include <doctest.h>
 
+#include <nodec_scene_serialization/entity_serializer.hpp>
+
+#include <cereal/cereal.hpp>
+
+TEST_CASE("testing serialization.") {
+    using namespace nodec_scene;
+    using namespace nodec_scene_serialization;
+
+    SceneSerialization serialization;
+
+    Scene scene;
+
+    auto root = scene.create_entity("root");
+    {
+        auto child = scene.create_entity("child1");
+        scene.hierarchy_system().append_child(root, child);
+    }
+    {
+        auto child = scene.create_entity("child2");
+        scene.hierarchy_system().append_child(root, child);
+    }
+
+    auto serializable_root = EntitySerializer(root, scene, serialization).serialize();
+    CHECK(serializable_root);
+
+    CHECK(serializable_root->children.size() == 2);
+}
