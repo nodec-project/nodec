8a6402f260ffda24dd02fa0c32436bf23ded9463
IOE <fivetwothreesix@gmail.com>
2023-06-03 18:11:18 +0900

    add arithmetic operations.

--- nodec/tests/vector/vector.cpp
+++ nodec/tests/vector/vector.cpp
@@ -6,16 +6,18 @@
 #include <nodec/vector4.hpp>
 
 
-template<typename T>
-void test_operator_plus(const T& a, const T& b, const T& expected) {
-    CHECK(a + b == expected);
-    CHECK(b + a == expected);
-}
-
 TEST_CASE("testing operator+") {
     using namespace nodec;
 
-    test_operator_plus(Vector2f(0.f, 1.f), Vector2f(10.f, 11.f), Vector2f(10.f, 12.f));
-    test_operator_plus(Vector3f(0.f, 1.f, 2.f), Vector3f(10.f, 11.f, 12.f), Vector3f(10.f, 12.f, 14.f));
-    test_operator_plus(Vector4f(0.f, 1.f, 2.f, 3.f), Vector4f(10.f, 11.f, 12.f, 13.f), Vector4f(10.f, 12.f, 14.f, 16.f));
+    CHECK_EQ(Vector2f(0.f, 1.f) + Vector2f(10.f, 11.f), Vector2f(10.f, 12.f));
+    CHECK_EQ(Vector3f(0.f, 1.f, 2.f) + Vector3f(10.f, 11.f, 12.f), Vector3f(10.f, 12.f, 14.f));
+    CHECK_EQ(Vector4f(0.f, 1.f, 2.f, 3.f) + Vector4f(10.f, 11.f, 12.f, 13.f), Vector4f(10.f, 12.f, 14.f, 16.f));
+}
+
+TEST_CASE("testing constants") {
+    using namespace nodec;
+
+    CHECK(Vector2f::ones == Vector2f(1.f, 1.f));
+    CHECK(Vector3f::ones == Vector3f(1.f, 1.f, 1.f));
+    CHECK(Vector4f::ones == Vector4f(1.f, 1.f, 1.f, 1.f));
 }
