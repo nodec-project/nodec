6e6d9acab13da88853be918b60d32a2d350aa358
IOE <fivetwothreesix@gmail.com>
2024-08-11 02:17:19 +0900

    CHANGE: Update cxx17

--- include/nodec/vector3.hpp
+++ include/nodec/vector3.hpp
@@ -20,23 +20,17 @@ public:
     };
 
 public:
-    Vector3();
-    Vector3(T x, T y, T z);
+    constexpr Vector3();
+    constexpr Vector3(T x, T y, T z);
 
     template<typename U>
-    explicit Vector3(const Vector3<U> &vector);
+    constexpr explicit Vector3(const Vector3<U> &vector);
 
     void set(T x, T y, T z);
 
 public:
     static const Vector3<T> zero;
     static const Vector3<T> ones;
-    // static const Vector3<T> forward;
-    // static const Vector3<T> back;
-    // static const Vector3<T> up;
-    // static const Vector3<T> down;
-    // static const Vector3<T> right;
-    // static const Vector3<T> left;
 };
 
 using Vector3f = Vector3<float>;
@@ -44,22 +38,22 @@ using Vector3i = Vector3<int>;
 using Vector3d = Vector3<double>;
 
 template<typename T>
-const Vector3<T> Vector3<T>::zero(0, 0, 0);
+const Vector3<T> Vector3<T>::zero{0, 0, 0};
 
 template<typename T>
-const Vector3<T> Vector3<T>::ones(1, 1, 1);
+const Vector3<T> Vector3<T>::ones{1, 1, 1};
 
 template<typename T>
-inline Vector3<T>::Vector3()
+inline constexpr Vector3<T>::Vector3()
     : x(0), y(0), z(0) {}
 
 template<typename T>
-inline Vector3<T>::Vector3(T x, T y, T z)
+inline constexpr Vector3<T>::Vector3(T x, T y, T z)
     : x(x), y(y), z(z) {}
 
 template<typename T>
 template<typename U>
-inline Vector3<T>::Vector3(const Vector3<U> &vector)
+inline constexpr Vector3<T>::Vector3(const Vector3<U> &vector)
     : x(static_cast<T>(vector.x)),
       y(static_cast<T>(vector.y)),
       z(static_cast<T>(vector.z)) {
@@ -73,12 +67,12 @@ inline void Vector3<T>::set(T _x, T _y, T _z) {
 }
 
 template<typename T>
-inline Vector3<T> operator-(const Vector3<T> &right) {
+inline constexpr Vector3<T> operator-(const Vector3<T> &right) {
     return Vector3<T>(-right.x, -right.y, -right.z);
 }
 
 template<typename T>
-inline Vector3<T> &operator+=(Vector3<T> &left, const Vector3<T> &right) {
+inline constexpr Vector3<T> &operator+=(Vector3<T> &left, const Vector3<T> &right) {
     left.x += right.x;
     left.y += right.y;
     left.z += right.z;
@@ -87,7 +81,7 @@ inline Vector3<T> &operator+=(Vector3<T> &left, const Vector3<T> &right) {
 }
 
 template<typename T>
-inline Vector3<T> &operator-=(Vector3<T> &left, const Vector3<T> &right) {
+inline constexpr Vector3<T> &operator-=(Vector3<T> &left, const Vector3<T> &right) {
     left.x -= right.x;
     left.y -= right.y;
     left.z -= right.z;
@@ -96,22 +90,22 @@ inline Vector3<T> &operator-=(Vector3<T> &left, const Vector3<T> &right) {
 }
 
 template<typename T>
-inline Vector3<T> operator+(const Vector3<T> &left, const Vector3<T> &right) {
+inline constexpr Vector3<T> operator+(const Vector3<T> &left, const Vector3<T> &right) {
     return Vector3<T>(left.x + right.x, left.y + right.y, left.z + right.z);
 }
 
 template<typename T>
-inline Vector3<T> operator-(const Vector3<T> &left, const Vector3<T> &right) {
+inline constexpr Vector3<T> operator-(const Vector3<T> &left, const Vector3<T> &right) {
     return Vector3<T>(left.x - right.x, left.y - right.y, left.z - right.z);
 }
 
 template<typename T>
-inline Vector3<T> operator*(const Vector3<T> &left, const Vector3<T> &right) {
+inline constexpr Vector3<T> operator*(const Vector3<T> &left, const Vector3<T> &right) {
     return Vector3<T>(left.x * right.x, left.y * right.y, left.z * right.z);
 }
 
 template<typename T>
-inline Vector3<T> &operator*=(Vector3<T> &left, const Vector3<T> &right) {
+inline constexpr Vector3<T> &operator*=(Vector3<T> &left, const Vector3<T> &right) {
     left.x *= right.x;
     left.y *= right.y;
     left.z *= right.z;
@@ -120,17 +114,17 @@ inline Vector3<T> &operator*=(Vector3<T> &left, const Vector3<T> &right) {
 }
 
 template<typename T>
-inline Vector3<T> operator*(const Vector3<T> &left, T right) {
+inline constexpr Vector3<T> operator*(const Vector3<T> &left, T right) {
     return Vector3<T>(left.x * right, left.y * right, left.z * right);
 }
 
 template<typename T>
-inline Vector3<T> operator*(T left, const Vector3<T> &right) {
+inline constexpr Vector3<T> operator*(T left, const Vector3<T> &right) {
     return Vector3<T>(left * right.x, left * right.y, left * right.z);
 }
 
 template<typename T>
-inline Vector3<T> &operator*=(Vector3<T> &left, T right) {
+inline constexpr Vector3<T> &operator*=(Vector3<T> &left, T right) {
     left.x *= right;
     left.y *= right;
     left.z *= right;
@@ -139,12 +133,12 @@ inline Vector3<T> &operator*=(Vector3<T> &left, T right) {
 }
 
 template<typename T>
-inline Vector3<T> operator/(const Vector3<T> &left, T right) {
+inline constexpr Vector3<T> operator/(const Vector3<T> &left, T right) {
     return Vector3<T>(left.x / right, left.y / right, left.z / right);
 }
 
 template<typename T>
-inline Vector3<T> &operator/=(Vector3<T> &left, T right) {
+inline constexpr Vector3<T> &operator/=(Vector3<T> &left, T right) {
     left.x /= right;
     left.y /= right;
     left.z /= right;
@@ -153,12 +147,12 @@ inline Vector3<T> &operator/=(Vector3<T> &left, T right) {
 }
 
 template<typename T>
-inline bool operator==(const Vector3<T> &left, const Vector3<T> &right) {
+inline constexpr bool operator==(const Vector3<T> &left, const Vector3<T> &right) {
     return (left.x == right.x) && (left.y == right.y) && (left.z == right.z);
 }
 
 template<typename T>
-inline bool operator!=(const Vector3<T> &left, const Vector3<T> &right) {
+inline constexpr bool operator!=(const Vector3<T> &left, const Vector3<T> &right) {
     return (left.x != right.x) || (left.y != right.y) || (left.z != right.z);
 }
 
--- tests/vector/vector.cpp
+++ tests/vector/vector.cpp
@@ -6,7 +6,7 @@
 #include <nodec/vector4.hpp>
 
 
-TEST_CASE("testing operator+") {
+TEST_CASE("Testing operator+") {
     using namespace nodec;
 
     CHECK_EQ(Vector2f(0.f, 1.f) + Vector2f(10.f, 11.f), Vector2f(10.f, 12.f));
@@ -14,10 +14,16 @@ TEST_CASE("testing operator+") {
     CHECK_EQ(Vector4f(0.f, 1.f, 2.f, 3.f) + Vector4f(10.f, 11.f, 12.f, 13.f), Vector4f(10.f, 12.f, 14.f, 16.f));
 }
 
-TEST_CASE("testing constants") {
+TEST_CASE("Testing constants") {
     using namespace nodec;
 
     CHECK(Vector2f::ones == Vector2f(1.f, 1.f));
     CHECK(Vector3f::ones == Vector3f(1.f, 1.f, 1.f));
     CHECK(Vector4f::ones == Vector4f(1.f, 1.f, 1.f, 1.f));
+}
+
+TEST_CASE("Testing constructor") {
+    using namespace nodec;
+
+    static_assert(Vector3f(1.f, 2.f, 3.f).x == 1.f);
 }
